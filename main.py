9e8ecb48bdeffba2443af39e8a09eab1867dbeab
Pawel-Robert <78816397+Pawel-Robert@users.noreply.github.com>
2021-03-19 15:27:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import numpy as np
 
 height = 10
 width = 10
-player = 1
 
 #tworzymy środowisko gry
-env = KiKEnv(height, width, player)
+env = KiKEnv(height, width)
 n_actions = env.height * env.width
 
-#funkcja tworząca modele o odpowiedniej architekturze
+#funkcja tworząca sieć neuronową o odpowiedniej architekturze
 def create_kik_model():
   model = tf.keras.models.Sequential([
-      # First Dense layer
-      tf.keras.layers.Dense(units=32, activation='relu'),
-      tf.keras.layers.Dense(units=n_actions, activation=None)
+          # na początku tworzymy kilka warstw konwolucyjnych przetwarzających planszę
+          tf.keras.layers.Conv2D(filters=24, kernel_size=(3, 3), activation=tf.nn.relu),
+          tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+          tf.keras.layers.Conv2D(filters=36, kernel_size=(3, 3), activation=tf.nn.relu),
+          tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+          # spłaszczamy sieć
+          tf.keras.layers.Flatten(),
+          # warstwa z maksymalną ilością połączeń
+          tf.keras.layers.Dense(128, activation=tf.nn.relu),
+          # ostatnia warstwa dająca prawdopodobieństwa wyboru poszczególnych pól na planszy
+          tf.keras.layers.Dense(n_actions, activation=tf.nn.softmax)
   ])
   return model
 
@@ -55,7 +62,7 @@ class Memory:
         self.rewards.append(new_reward)
 
 
-# Helper function to combine a list of Memory objects into a single Memory.
+# Funkcja pomocnicza agrgująca pamięć
 #     This will be very useful for batching.
 def aggregate_memories(memories):
     batch_memory = Memory()
@@ -67,8 +74,13 @@ def aggregate_memories(memories):
     return batch_memory
 
 
-# Instantiate a single Memory buffer
-memory = Memory()
+# Tworzymy dwie instancje klasy pamięci, po jednej dla każedego z graczy
+
+# pamięć gracza -1
+memory_0 = Memory()
+
+# pamięć gracza 1
+memory_1 = Memory()
 
 # funkcja normalizująca zmienną x
 def normalize(x):
@@ -97,7 +109,7 @@ def discount_rewards(rewards, gamma=0.95):
 def compute_loss(logits, actions, rewards):
     neg_logprob = tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=logits, labels=actions)
-    loss = tf.reduce_mean(neg_logprob * rewards)  # TODO
+    loss = tf.reduce_mean(neg_logprob * rewards) 
     return loss
 
 # funckja trenująca model na podstawie wybranej akcji oraz obserwacji
@@ -109,54 +121,10 @@ def train_step(model, optimizer, observations, actions, discounted_rewards):
   grads = tape.gradient(loss, model.trainable_variables) 
   optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-  
-  # Learning rate and optimizer
-  learning_rate = 1e-3
-  optimizer = tf.keras.optimizers.Adam(learning_rate)
-
-  # instantiate kik agent
-  kik_model = create_kik_model()
-
-  # to track our progress
-  smoothed_reward = mdl.util.LossHistory(smoothing_factor=0.9)
-
-  if hasattr(tqdm, '_instances'): tqdm._instances.clear()  # clear if it exists
-  for i_episode in range(500):
-
-      plotter.plot(smoothed_reward.get())
-
-      # Restart the environment
-      observation = env.reset()
-      memory.clear()
-
-      while True:
-          # using our observation, choose an action and take it in the environment
-          action = choose_action(kik_model, observation)
-          next_observation, reward, done, info = env.step(action)
-          # add to memory
-          memory.add_to_memory(observation, action, reward)
-
-          # is the episode over? did you crash or do so well that you're done?
-          if done:
-              # determine total reward and keep a record of this
-              total_reward = sum(memory.rewards)
-              smoothed_reward.append(total_reward)
-
-              # initiate training - remember we don't know anything about how the
-              #   agent is doing until it has crashed!
-              train_step(kik_model, optimizer,
-                         observations=np.vstack(memory.observations),
-                         actions=np.array(memory.actions),
-                         discounted_rewards=discount_rewards(memory.rewards))
-
-              # reset the memory
-              memory.clear()
-              break
-          # update our observatons
-          observation = next_observation
 
 
-### kik training! ###
+
+### Trenujemy model grający w KiK ###
 
 # Learning rate and optimizer
 learning_rate = 1e-3
@@ -165,41 +133,67 @@ optimizer = tf.keras.optimizers.Adam(learning_rate)
 # instantiate kik agent
 kik_model = create_kik_model()
 
-# to track our progress
+# to track our progress -> funkcje z pakietu MIT
 smoothed_reward = mdl.util.LossHistory(smoothing_factor=0.9)
 plotter = mdl.util.PeriodicPlotter(sec=2, xlabel='Iterations', ylabel='Rewards')
 
-if hasattr(tqdm, '_instances'): tqdm._instances.clear()  # clear if it exists
+if hasattr(tqdm, '_instances'): tqdm._instances.clear()  # co to znaczy? (clear if exists)
 for i_episode in range(500):
 
     plotter.plot(smoothed_reward.get())
 
     # Restart the environment
     observation = env.reset()
-    memory.clear()
+    memory_0.clear()
+    memory_1.clear()
 
     while True:
+        # zmieniamy gracza wykonującego ruch (inwolucja graczy)
+        player = - player
         # using our observation, choose an action and take it in the environment
-        action = choose_action(kik_model, observation)
+        raw_action = choose_action(kik_model, observation)
+        # zamieniamy "surową akcję" na współrzędne pola
+        action[0] = raw_action/env.width
+        action[1] = raw_action - action[0]*env.width
+        action[2] = player
+        # posługując się naszą akcją wykonujemy krok w środowisku i zbieramy z niego informacje
         next_observation, reward, done, info = env.step(action)
-        # add to memory
-        memory.add_to_memory(observation, action, reward)
+        # aktualizujemy pamięć w zależności od tego, który gracz wykonał ruch
+        if player = -1:
+            memory_0.add_to_memory(observation, action, reward)
+            # jeśli wygrał gracz numer -1
+            if done:
+                # determine total reward and keep a record of this
+                total_reward = sum(memory_0.rewards)
+                smoothed_reward.append(total_reward)
 
-        # is the episode over? did you crash or do so well that you're done?
-        if done:
-            # determine total reward and keep a record of this
-            total_reward = sum(memory.rewards)
-            smoothed_reward.append(total_reward)
+                # initiate training - remember we don't know anything about how the
+                #   agent is doing until it has crashed!
+                train_step(kik_model, optimizer,
+                           observations=np.vstack(memory_0.observations),
+                           actions=np.array(memory_0.actions),
+                           discounted_rewards=discount_rewards(memory_0.rewards))
 
-            # initiate training - remember we don't know anything about how the
-            #   agent is doing until it has crashed!
-            train_step(kik_model, optimizer,
-                       observations=np.vstack(memory.observations),
-                       actions=np.array(memory.actions),
-                       discounted_rewards=discount_rewards(memory.rewards))
+                # reset the memory
+                memory_0.clear()
+                break
+        else:
+            memory_1.add_to_memory(observation, action, reward)
+            # jeśli wygrał gracz numer 1
+            if done:
+                # determine total reward and keep a record of this
+                total_reward = sum(memory_1.rewards)
+                smoothed_reward.append(total_reward)
 
-            # reset the memory
-            memory.clear()
-            break
+                # initiate training - remember we don't know anything about how the
+                #   agent is doing until it has crashed!
+                train_step(kik_model, optimizer,
+                           observations=np.vstack(memory_1.observations),
+                           actions=np.array(memory_1.actions),
+                        discounted_rewards=discount_rewards(memory_1.rewards))
+
+                # reset the memory
+                memory_1.clear()
+                break
         # update our observatons
         observation = next_observation
