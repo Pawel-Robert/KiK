b66b13f81df6508bb4b6cdce5e95061c233d2b67
Pawel-Robet <pc@grcconsulting.pl>
2021-04-01 14:30:59 +0200

    after first test, long prediction time

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-from KiK.value_network import ValueNetwork
+#from KiK.value_network import ValueNetwork
 from kik_env import KiKEnv
+from q_network import QNetwork3x3
 import numpy as np
+from runner import Runner
+from agent import Small_Agent_Explorator, Small_Agent
+
 
 width = 3
 height = 3
 winning_condition = 3
 
-env = KiKEnv(width, height, winning_condition)
+network = QNetwork3x3()
 
-ValueNetwork(3, 3, None)
+env = KiKEnv(width, height, winning_condition)
+#    Runner(agent_class, network, epsilon env, buffer_size, time_limit):
+runner = Runner(Small_Agent_Explorator, network, 0.1, env, 1, 10)
+
+#  run(n_iterations, episodes_in_batch, data_size, epochs)
+# n_iterations = number of training processes
+# episodes_in_batch = number of games played for one training
+runner.run(1, 10, 100, 1)
+
+env.reset()
+
+#agent = Small_Agent(network)
+#while True:
+#    env.human_vs_ai_play(agent)
+
+""" JAK UŻYĆ SIECI 3X3 """
+# st = np.zeros(9)
+# st[0] = 1.
+# st_input = np.array([st])
+# ac = np.zeros(9)
+# ac[2] = -1.
+# ac_input = np.array([ac])
+# d = QNetwork3x3()
+# print(d.model.predict([st_input, ac_input]))
+
+
+
+
+
+#ValueNetwork(3, 3, None)
 
 # trajectory = env.random_play()
 
 # print(trajectory)
 
-env.human_vs_human_play()
+#env.human_vs_human_play()
--- runner.py
+++ runner.py
@@ -1,61 +1,81 @@
 from experience_buffer import ExperienceBuffer
 from metrics import batch_metrics
+import numpy as np
+from datetime import datetime
+
+import multiprocessing as mp
+
 
 
 class Runner:
-    def __init__(self, agent_class, model, env, buffer_size, time_limit):
+    def __init__(self, agent_class, network, epsilon, env, buffer_size, time_limit):
         """The main class to run the training loop"""
         # tworzymy agenta o typie podanym w parametrze agent_class
-        self.model = model
-        self.agent = agent_class(self.model)
+        self.network = network
+        self.agent = agent_class(network, epsilon)
         self.env = env
         self.time_limit = time_limit
+        self.N = 0
 
         self.buffer = ExperienceBuffer(buffer_size)
 
     def run_one_episode(self):
         """Plays one game and returns trajectory"""
-        trajectory = None
-        time = 0
+        trajectory = []
+        t = 0
+        self.N += 1
+        #now = datetime.now().time()
+        #print(now.strftime("%H:%M:%S"))
         observation = self.env.reset()
         while True:
             # agent wybiera akcję na polu jeszcze nie zajętym
-            while True:
-                action = self.agent.act(observation, self.env.legal_actions())
-                if self.env.is_allowed_move(action):
-                    break
+            if self.env.legal_actions():
+                action, q_value = self.agent.act(observation, self.env.legal_actions(), self.N)
+            else:
+                break
             # wybraną akcją wpływamy na środowiski i zbieramy obserwacje
             next_observation, reward, done, info = self.env.step(action)
+
             # obserwacje dodajemy do trajektorii
-            modified_action = np.zeros(env.heigth, env.width)
-            modified_action[action // env.width, action % env.width] = - env.player
-            trajectory.append(observation, modified_action, reward, done)
+            trajectory.append([observation, action, q_value, reward, done])
             if done:
                 break
             # update our observatons
             observation = next_observation
             # sprawdzmy, czy nie przekroczyliśmy limitu czasu
-            time += 1
-            if time == self.time_limit:
+            t += 1
+            if t == self.time_limit:
                 break
+        # self.env.render()
+        # print(done)
         return trajectory
 
     def run_batch_of_episodes(self, n_episodes):
         """This could be parallelized to run on many cpus"""
+        #pool = mp.Pool(mp.cpu_count())
+        trajectory_batch = []
+        #batch_objects = [pool.apply_async(self.run_one_episode, args=(i)) for i in range(n_episodes)]
+        #trajectory_batch = [r.get()[1] for r in batch_objects]
+        #pool.close()
         trajectory_batch = [self.run_one_episode() for _ in range(n_episodes)]
+        #print(trajectory_batch)
+        #trajectory_batch = []
+        #result_objects = [pool.apply_async(howmany_within_range2, args=(i, row, 4, 8)) for i, row in enumerate(data)]
+        #results = [r.get()[1] for r in result_objects]
         return trajectory_batch
 
     def run(self, n_iterations, episodes_in_batch, data_size, epochs):
         """Full RL training loop"""
         for num in range(n_iterations):
+            self.buffer.clear_buffer()
             print(f'Processing step = {num}')
             trajectory_batch = self.run_batch_of_episodes(episodes_in_batch)
-            print(batch_metrics(trajectory_batch, num))
+            print(f'Win rate = {batch_metrics(trajectory_batch, num)}')
             for trajectory in trajectory_batch:
                 self.buffer.add_trajectory(trajectory)
             x, y = self.buffer.prepare_training_data(data_size)
-            train_metrics = self.model.fit(x, y, epochs)
-            print(train_metrics)
+            train_metrics = self.network.model.fit(x, y, epochs)
+
 
         print(f'Training finished.')
 
